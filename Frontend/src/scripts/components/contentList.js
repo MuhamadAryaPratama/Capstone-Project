519807b03bbe40320f610ca03366d4c65b5ba14f
MuhamadAryaPratama <muharya0203@gmail.com>
2024-06-05 20:11:30 +0700

    Melakuka integrasi

--- Frontend/src/scripts/components/contentList.js
+++ Frontend/src/scripts/components/contentList.js
@@ -16,6 +16,36 @@ class contentList extends HTMLElement {
             <option value="" selected>Pilih Provinsi</option>
             <option value="aceh">Nanggroe Aceh Darussalam</option>
             <option value="sumut">Sumatera Utara</option>
+            <option value="sumsel">Sumatera Selatan</option>
+            <option value="sumbar">Sumatera Barat</option>
+            <option value="bengkulu">Bengkulu</option>
+            <option value="riau">Riau</option>
+            <option value="kepulauanriau">Kepulauan Riau</option>
+            <option value="jambi">Jambi</option>
+            <option value="lampung">Lampung</option>
+            <option value="bangkabelitung">Bangka Belitung</option>
+            <option value="banten">Banten</option>
+            <option value="jakarta">DKI Jakarta</option>
+            <option value="jawabarat">Jawa Barat</option>
+            <option value="jawatengah">Jawa Tengah</option>
+            <option value="yogyakarta">Daerah Istimewa Yogyakarta</option>
+            <option value="jawatimur">Jawa Timur</option>
+            <option value="bali">Bali</option>
+            <option value="ntt">Nusa Tenggara Timur</option>
+            <option value="ntb">Nusa Tenggara Barat</option>
+            <option value="gorontalo">Gorontalo</option>
+            <option value="sulawesibarat">Sulawesi Barat</option>
+            <option value="sulawesitengah">Sulawesi Tengah</option>
+            <option value="sulawesiutara">Sulawesi Utara</option>
+            <option value="sulawesitenggara">Sulawesi Tenggara</option>
+            <option value="sulawesiselatan">Sulawesi Selatan</option>
+            <option value="kalimantanbarat">Kalimantan Barat</option>
+            <option value="kalimantantimur">Kalimantan Timur</option>
+            <option value="kalimantanselatan">Kalimantan Selatan</option>
+            <option value="kalimantantengah">Kalimantan Tengah</option>
+            <option value="kalimantanutara">Kalimantan Utara</option>
+            <option value="maluku">Maluku</option>
+            <option value="malukuutara">Maluku Utara</option>
             <!-- Add more options here for other provinces -->
           </select>
           <button class="btn btn-primary" id="exploreButton">Jelajahi</button>
@@ -25,18 +55,18 @@ class contentList extends HTMLElement {
   }
 
   addEventListeners() {
-    const exploreButton = this.querySelector("#exploreButton");
-    const provinceSelect = this.querySelector("#provinceSelect");
+    const exploreButton = this.querySelector('#exploreButton');
+    const provinceSelect = this.querySelector('#provinceSelect');
 
-    exploreButton.addEventListener("click", () => {
+    exploreButton.addEventListener('click', () => {
       const selectedProvince = provinceSelect.value;
       if (selectedProvince) {
         window.location.href = `#${selectedProvince}`;
       } else {
-        alert("Silakan pilih provinsi terlebih dahulu!");
+        alert('Silakan pilih provinsi terlebih dahulu!');
       }
     });
   }
 }
 
-customElements.define("content-list", contentList);
+customElements.define('content-list', contentList);
---
+++ Frontend/src/scripts/components/malukuMenu.js
@@ -0,0 +1,53 @@
+class MalukuMenu extends HTMLElement {
+    connectedCallback() {
+      this.fetchData();
+    }
+  
+    async fetchData() {
+      try {
+        const response = await fetch("http://localhost:4000/maluku");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        this.render(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+        this.renderError(error);
+      }
+    }
+  
+    render(menus) {
+      this.innerHTML = `
+          <div class="container">
+            <h1>Sumatera Selatan Menu</h1>
+            <ul>
+              ${menus
+                .map(
+                  (menu) => `
+                <li>
+                  <h2>Nama Restoran: ${menu.restaurant_name}</h2>
+                  <p>Deskripsi: ${menu.description}</p>
+                  <p>Link Google Maps: <a href="${menu.google_maps_link}" target="_blank">Google Maps</a></p>
+                  <p>Rating: ${menu.rating}</p>
+                </li>
+              `
+                )
+                .join("")}
+            </ul>
+          </div>
+        `;
+    }
+  
+    renderError(error) {
+      this.innerHTML = `
+          <div class="container">
+            <h1>Error Loading Sumatera Selatan Menu</h1>
+            <p>Error: ${error.message}</p>
+          </div>
+        `;
+    }
+  }
+  
+  customElements.define("maluku-menu", MalukuMenu);
+  
