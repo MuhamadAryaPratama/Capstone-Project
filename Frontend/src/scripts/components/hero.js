e5994404af650d3e3dd9569607eadfcd650c0c64
tanjeaux <146642203+tanjeaux@users.noreply.github.com>
2024-06-20 14:20:52 +0700

    Menambahkan href untuk button Jelajahi pada Hero

--- Frontend/src/scripts/components/hero.js
+++ Frontend/src/scripts/components/hero.js
@@ -9,7 +9,7 @@ class Hero extends HTMLElement {
                 <div class="hero__inner">
                 <h1 class="hero__title">Membantu Temukan <br> Rasa Paling Enak</h1>
                 <p class="hero__tagline"><span class="bold-text">NusantaRasa</span> hadir untuk temukan rasa terbaik untukmu <br> dari seluruh kuliner yang ada Indonesia</p>
-                <button>Jelajahi</button>
+                <a href="#jelajahi"><button>Jelajahi</button></a>
                 </div>
                 
             <div class="hero-img-container">
